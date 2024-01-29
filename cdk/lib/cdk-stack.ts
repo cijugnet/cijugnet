d5c910b1ef54b9b86d462233c8db2b36929c968b
cfunk6 <cfunk@sourceallies.com>
2024-01-29 14:45:25 -0600

    create s3 buckets and upload app html files add cdk diff and synth

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,89 @@
 import * as cdk from 'aws-cdk-lib';
+import { AccountPrincipal, AnyPrincipal, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Bucket, BucketEncryption, ObjectOwnership, RedirectProtocol } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const s3ServerLogsBucket = new Bucket(this, 'cijug-server-logs-bucket', {
+      bucketName: 'cijug-site-server-logs',
+      encryption: BucketEncryption.S3_MANAGED,
+      objectOwnership: ObjectOwnership.OBJECT_WRITER
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    s3ServerLogsBucket.addToResourcePolicy(
+      new PolicyStatement({
+        resources: [
+          s3ServerLogsBucket.arnForObjects("*"),
+          s3ServerLogsBucket.bucketArn
+        ],
+        actions: ["s3:List*", "S3:Get*"],
+        principals: [new AccountPrincipal(this.account)],
+      })
+    );
+
+    const s3redirectBucket = new Bucket(this, 'cijug-redirect-bucket', {
+      bucketName: 'cijug.net',
+      encryption: BucketEncryption.S3_MANAGED,
+      objectOwnership: ObjectOwnership.OBJECT_WRITER,
+      websiteRedirect: {
+        hostName: 'www.cijug.net',
+        protocol: RedirectProtocol.HTTPS
+      },
+      blockPublicAccess: {
+		    blockPublicPolicy: false,
+		    blockPublicAcls: false,
+		    ignorePublicAcls: false,
+		    restrictPublicBuckets: false,
+	    }
+    });
+
+    s3redirectBucket.addToResourcePolicy(
+      new PolicyStatement({
+        resources: [
+          s3redirectBucket.arnForObjects("*"),
+          s3redirectBucket.bucketArn
+        ],
+        actions: ["s3:List*", "S3:Get*"],
+        principals: [new AnyPrincipal()],
+      })
+    );
+
+    const s3bucket = new Bucket(this, 'cijug-bucket', {
+      bucketName: 'www.cijug.net',
+      encryption: BucketEncryption.S3_MANAGED,
+      websiteIndexDocument: 'index.html',
+      objectOwnership: ObjectOwnership.OBJECT_WRITER,
+      versioned: true,
+	    blockPublicAccess: {
+		    blockPublicPolicy: false,
+		    blockPublicAcls: false,
+		    ignorePublicAcls: false,
+		    restrictPublicBuckets: false,
+	    },
+      serverAccessLogsBucket: s3ServerLogsBucket
+    });
+
+    s3bucket.addToResourcePolicy(
+      new PolicyStatement({
+        resources: [
+          s3bucket.arnForObjects("*"),
+          s3bucket.bucketArn
+        ],
+        actions: ["s3:List*", "S3:Get*"],
+        principals: [new AnyPrincipal()]
+      })
+    );
+
+    new BucketDeployment(this, 'cijug-bucket-deployment', {
+      sources: [Source.asset('../app')],
+      destinationBucket: s3bucket
+    });
+
+
+    
   }
 }
